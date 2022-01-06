a7262ab04c4a9322294c35bbb0f13bf67abdef0c
JojoDuke <jojoamankwa@gmail.com>
2022-01-06 23:10:42 +0000

    tiree

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
-import CardsList from './Components/CardsList';
-import Inputs from './Components/Inputs';
+import CardItem from './Components/CardItem';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
+  const [newCard, setNewCard] = useState([]);
+
+  const createCard = (e) => {
+    setNewCard(newCard.concat(<CardItem key={newCard.length}/>));
+  }
+
   return (
     <div className="main">
       <div className="block">
         <h2 style={{ marginLeft: "20px", marginTop: "10px", }}>Midas</h2>
-        <Inputs/>
+        
+        <div>
+            <form className="inputForm">
+                <input type="text" placeholder="Name of Card"/>
+                <input type="text" placeholder="Mobile Money Number"/>
+                <input type="text" placeholder="Amount"/>
+                <button className="card-btn" onClick={createCard}>Create Card</button>
+            </form>
+        </div>
       </div>
       <div className="cards-div">
-        <CardsList/>
+        {newCard}
       </div>
     </div>
   );
--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -3,11 +3,17 @@ import './CardItem.css'
 function CardItem() {
     return (
         <div className='card-div'>
-            Dollar amount
-            Card no.
-            Name
-            Expiry date
-            CSV no.s
+            <div style={{ display: "flex", justifyContent: "space-between", padding: "0px 20px", }}>
+                <h4>Dollar amount</h4>
+                <h4>CSV</h4>
+            </div>
+
+            <h1 style={{ margin: "auto", }}>Card no.</h1>
+            
+            <div style={{ display: "flex", justifyContent: "space-between", padding: "0px 20px", }}>
+                <h4>Name</h4>
+                <h4>Expiry</h4>
+            </div>
         </div>
     )
 }
